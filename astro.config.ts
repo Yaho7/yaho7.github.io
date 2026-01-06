b4c5fcb84910e6228a54456631d6843faef3e08d
yaho <1542754972@qq.com>
2026-01-06 23:07:59 +0800

    first commit

--- astro.config.ts
+++ astro.config.ts
@@ -15,7 +15,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: "https://yaho7.github.io",
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
